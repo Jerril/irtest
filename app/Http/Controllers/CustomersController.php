6301962433e1bfe5a0251d92d84e0c55da6da3c2
Jeremiah Ekundayo <dayoolapeju@outlook.com>
2022-01-29 12:19:17 +0100

    fetch all customers

---
+++ app/Http/Controllers/CustomersController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Customer;
+use App\Http\Requests\StoreCustomerRequest;
+use App\Http\Requests\UpdateCustomerRequest;
+
+class CustomersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // return response()->json([
+        //     'data' => [
+
+        //     ]
+        // ])
+        
+        return response()->json(Customer::all());
+        // return Customer::all();
+        // return "get all customers";
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        // Get the form submission
+        $request->validate([
+            'name'=>'required',
+            'email'=>'required'
+        ]);
+
+        $newCustomer = Customer::create($request->all());
+        
+        return response()->json([
+            "data" => [
+                "name" => $newCustomer->name,
+                "email" => $newCustomer->email
+            ]
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreCustomerRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCustomerRequest $request)
+    {
+        //
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+        ]);
+
+        Customer::create($request->all());
+
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateCustomerRequest  $request
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateCustomerRequest $request, Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Customer $customer)
+    {
+        //
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomersController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Fetch all customers
+Route::get('/customers', [CustomersController::class, 'index']);
+Route::post('/customers', [CustomersController::class, 'create']);
+Route::get('/customers/{id}', function($id) {
+    return "Give the customer details & its transactions";
+});
+Route::post('/customers/{id}/debit', function(Request $Request, $id){
+    return "this should debit the user & save the transaction details to";
+});
